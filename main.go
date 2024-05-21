8a5e410039b231de08cd6ba950e4169fde2349b4
binsabit <yerdaulet.seidizhappar@gmail.com>
2024-05-21 10:36:06 +0500

    main

--- main.go
+++ main.go
@@ -1,1 +1,57 @@
-package channels
+package main
+
+import (
+	image "github.com/binsabit/images/images"
+	"log"
+	"os"
+)
+
+func GetFiles(filenames []string) chan *os.File {
+	fileChan := make(chan *os.File)
+
+	go func() {
+		for _, filename := range filenames {
+			file, err := os.Open(filename)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			fileChan <- file
+		}
+		close(fileChan)
+	}()
+
+	return fileChan
+}
+
+func GetImages(fileChan chan *os.File, opts ...image.OptFunc) chan *image.Image {
+	imgChan := make(chan *image.Image, 10)
+	go func() {
+		for file := range fileChan {
+			img, err := image.NewImage(file.Name(), file, opts...)
+			if err != nil {
+				log.Println(err)
+			}
+			imgChan <- img
+		}
+		close(imgChan)
+	}()
+
+	return imgChan
+}
+
+func main() {
+	filename := []string{"./test1.jpeg", "./test2.jpeg"}
+
+	filesChan := GetFiles(filename)
+
+	getImages := GetImages(filesChan, image.Format(image.Webp))
+
+	for img := range getImages {
+		err := img.Save("results")
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+}
